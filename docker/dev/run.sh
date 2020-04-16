b55a21ede60233947d543bb97b178c3414db84bf
Shai Ayal <shaiay@gmail.com>
2020-04-16 13:17:26 +0300

    added ports and folders to run.sh

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,1 +1,11 @@
-docker run -d -v /etc/sshd:/etc/secret:readonly -p 8022:22
+docker run \
+-d \
+-p 8888:8888 \
+-p 8022:22 \
+--gpus all \
+--cap-add SYS_PTRACE \
+-v ~/notebooks:/notebooks \
+-v /data/opt:/opt \
+-v /etc/ssh:/etc/secret:ro \
+--env=DISPLAY \
+swift-dev
